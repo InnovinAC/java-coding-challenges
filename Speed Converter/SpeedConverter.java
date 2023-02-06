17d049729472f114d1a07c09c4650712125d6871
Innovin <102300150+InnovinAC@users.noreply.github.com>
2023-02-06 03:00:53 +0100

    Create SpeedConverter.java

---
+++ Speed Converter/SpeedConverter.java
@@ -0,0 +1,19 @@
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+        if (milesPerHour == -1) {
+            System.out.print("Invalid Value");
+            return;
+        }
+        System.out.print(kilometersPerHour +
+                " km/h = " + milesPerHour + " mi/h");
+
+    }
+}
